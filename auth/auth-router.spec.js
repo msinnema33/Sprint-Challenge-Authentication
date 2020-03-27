175aea632ad45924805e2775294968e62702e463
Mike Sinnema <msinnema33@gmail.com>
2020-03-27 10:28:38 -0700

    working on auth test for register

---
+++ auth/auth-router.spec.js
@@ -0,0 +1,28 @@
+const request = require("supertest");
+const server = require("../api/server.js");
+const test = "newValue1"
+describe("auth router", function() {
+  it("should run the tests", function() {
+    expect(true).toBe(true);
+  });
+  describe("POST /api/auth/register", () => {
+    it("should return 201 created", async () => {
+      const res = await request(server)
+        .post("/api/auth/register")
+        .send({
+          username: test,
+          password: test
+        });
+      expect(res.status).toBe(201);
+    });
+    it("should not register a user without a unique username", async () =>{
+      const res = await request(server)
+      .post("/api/auth/register")
+      .send({
+        username: "test",
+        password:"err"
+       });
+      expect(res.status).toBe(500) 
+    });
+})
+});
